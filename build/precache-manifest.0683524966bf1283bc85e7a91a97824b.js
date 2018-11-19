c0b964d5a245707b06ccada5e83fa2d7b46ecd47
triptishukla <tripti12shukla1280@gmail.com>
2018-11-19 13:35:12 +0530

    updated finally

---
+++ build/precache-manifest.0683524966bf1283bc85e7a91a97824b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/githubsearch/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8986f40e80ceb31647c1f3c051b9f02d",
+    "url": "/githubsearch/static/media/bg.8986f40e.png"
+  },
+  {
+    "revision": "c36f9cef5ed7766ad695",
+    "url": "/githubsearch/static/js/runtime~main.c36f9cef.js"
+  },
+  {
+    "revision": "2f9f39a516bf29a3a767",
+    "url": "/githubsearch/static/js/main.2f9f39a5.chunk.js"
+  },
+  {
+    "revision": "74588e2cf2dea9eaa01e",
+    "url": "/githubsearch/static/js/1.74588e2c.chunk.js"
+  },
+  {
+    "revision": "2f9f39a516bf29a3a767",
+    "url": "/githubsearch/static/css/main.9ae97384.chunk.css"
+  },
+  {
+    "revision": "d7a007b5c573e8b73ef821b65a1a6abc",
+    "url": "/githubsearch/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e264f0c8fc6e1d924a0083c1e51e367.js"
+  "/githubsearch/precache-manifest.0683524966bf1283bc85e7a91a97824b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/githubsearch/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
